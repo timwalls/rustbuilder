b7866fbdb589d131cd39f1556b59870c3d65161e
Tim Walls <tim.walls@snowgoons.com>
2023-04-13 13:57:19 +0300

    Use the Arm64 server to build arm/v7 (Pisicuta isn't up to building sccache in a reasonable time)

--- buildandpush.sh
+++ buildandpush.sh
@@ -3,12 +3,12 @@
 PLATFORMS=linux/amd64,linux/arm/v7,linux/arm64
 
 IMAGE=snowgoons/rustbuilder
-TAG=rustc-1.65.0
+TAG=rustc-1.68.2
 
 docker buildx rm rustbuilder-builder || true
 docker buildx create --name rustbuilder-builder
 docker buildx create --append --name rustbuilder-builder --platform linux/amd64   hp-node0
-docker buildx create --append --name rustbuilder-builder --platform linux/arm/v7  pisicuta
+docker buildx create --append --name rustbuilder-builder --platform linux/arm/v7  arm-node0
 docker buildx create --append --name rustbuilder-builder --platform linux/arm64   arm-node0
 
 docker buildx use rustbuilder-builder
